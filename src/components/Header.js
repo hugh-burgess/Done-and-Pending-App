fc707127b93d4c2ad263765cb478cad81804dadd
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-04-06 11:33:30 +0200

    length fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,7 +26,7 @@ export default function Header({ onAddToDo, setFilter }) {
       <form onSubmit={handleSubmit}>
         <input
           className="inputBox"
-          maxlength="12"
+          maxlength="11"
           id="input"
           name="input"
           type="text"
